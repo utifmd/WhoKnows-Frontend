a422e741313f1bfa710e90ea4712409d35cc7508
Utif Milkedori <utifmd@gmail.com>
2021-12-09 09:35:26 +0700

    adding participant usecases

---
+++ domain/usecase/src/main/java/com/dudegenuine/usecase/participant/GetParticipant.kt
@@ -0,0 +1,33 @@
+package com.dudegenuine.usecase.participant
+
+import com.dudegenuine.model.Participant
+import com.dudegenuine.model.Resource
+import com.dudegenuine.model.validation.HttpFailureException
+import com.dudegenuine.repository.contract.IParticipantRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+/**
+ * Thu, 09 Dec 2021
+ * WhoKnows by utifmd
+ **/
+class GetParticipant
+    @Inject constructor(
+        private val repository: IParticipantRepository) {
+        operator fun invoke(id: String): Flow<Resource<Participant>> = flow {
+            try {
+                emit(Resource.Loading())
+                val participant = repository.read(id)
+                emit(Resource.Success(participant))
+            } catch (e: HttpFailureException){
+                emit(Resource.Error(e.localizedMessage ?: Resource.HTTP_FAILURE_EXCEPTION))
+            } catch (e: HttpException){
+                emit(Resource.Error(e.localizedMessage ?: Resource.HTTP_EXCEPTION))
+            } catch (e: IOException){
+                emit(Resource.Error(Resource.IO_EXCEPTION))
+            }
+        }
+}
