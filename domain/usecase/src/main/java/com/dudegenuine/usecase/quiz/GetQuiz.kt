22292ce2747131b1283cd45a23fc4c5c894c41fa
Utif Milkedori <utifmd@gmail.com>
2021-12-09 09:13:58 +0700

    adding quiz usecases

---
+++ domain/usecase/src/main/java/com/dudegenuine/usecase/quiz/GetQuiz.kt
@@ -0,0 +1,33 @@
+package com.dudegenuine.usecase.quiz
+
+import com.dudegenuine.model.Quiz
+import com.dudegenuine.model.Resource
+import com.dudegenuine.model.validation.HttpFailureException
+import com.dudegenuine.repository.contract.IQuizRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+/**
+ * Thu, 09 Dec 2021
+ * WhoKnows by utifmd
+ **/
+class GetQuiz
+    @Inject constructor(
+    private val repository: IQuizRepository) {
+    operator fun invoke(id: String): Flow<Resource<Quiz>> = flow {
+        try {
+            emit(Resource.Loading())
+            val quiz = repository.read(id)
+            emit(Resource.Success(quiz))
+        } catch (e: HttpFailureException){
+            emit(Resource.Error(e.localizedMessage ?: Resource.HTTP_FAILURE_EXCEPTION))
+        } catch (e: HttpException){
+            emit(Resource.Error(e.localizedMessage ?: Resource.HTTP_EXCEPTION))
+        } catch (e: IOException){
+            emit(Resource.Error(Resource.IO_EXCEPTION))
+        }
+    }
+}
